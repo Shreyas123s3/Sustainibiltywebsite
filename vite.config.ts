2fb371a5cc7cd8da12f4713af36acc89083031c8
Shreyas123s3 <shreyassalunkhe807@gmail.com>
2025-07-15 09:35:52 +0530

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,29 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  optimizeDeps: {
+    exclude: ['lucide-react'],
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+          framer: ['framer-motion'],
+          charts: ['chart.js', 'react-chartjs-2'],
+          tensorflow: ['@tensorflow/tfjs', '@tensorflow-models/coco-ssd'],
+        },
+      },
+    },
+    chunkSizeWarningLimit: 1000,
+  },
+});
